a44b83a74b5c3ed55a6437783c9859d443db228e
DTDucas <dtducas@gmail.com>
2026-03-29 15:06:16 +0700

    ✨ feat(sorting) [BE]: implement basic sorting algorithm

---
+++ 026-DSA-PTIT-D02001-Sap_Xep_Co_Ban.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        // author: Trần Quang Dương aka DTDucas
+
+        for (int i = 0; i < n; i++) {
+            cout << a[i];
+            if (i != n - 1) cout << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
